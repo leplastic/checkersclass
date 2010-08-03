6754bf12728824ebd4c5e00dbfd33b98059b2bd6
asilva_sc@hotmail.com <asilva_sc@hotmail.com@6e8cf0fd-794f-984a-b2d7-fa5e9ce5254e>
2010-08-03 16:18:14 +0000

    first commit

---
+++ trunk/Piece.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Softklin.Checkers
+{
+    /// <summary>
+    /// Represents a piece in checkers game
+    /// </summary>
+    class Piece
+    {
+        #region Properties
+
+        /// <summary>
+        /// Gets the player how owns the current piece [read-only]
+        /// </summary>
+        public Player OwnerPlayer { get; private set; }
+
+        /// <summary>
+        /// Indicates whenever the current piece is queen
+        /// </summary>
+        public bool Queen { get; set; }
+
+        #endregion
+
+
+        /// <summary>
+        /// Creates a new piece
+        /// </summary>
+        /// <param name="owner">The player who owns this piece</param>
+        public Piece(Player owner)
+        {
+            this.OwnerPlayer = owner;
+            this.Queen = false;
+        }
+    }
+}
