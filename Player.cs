a8692f6d7acc8d6a1d4398cec5e8026261b31463
asilva_sc@hotmail.com <asilva_sc@hotmail.com@6e8cf0fd-794f-984a-b2d7-fa5e9ce5254e>
2010-08-03 16:18:14 +0000

    first commit

---
+++ Player.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Softklin.Checkers
+{
+    /// <summary>
+    /// Represents a player in checkers game
+    /// </summary>
+    class Player
+    {
+        #region Properties
+
+        /// <summary>
+        /// The player's nickname
+        /// </summary>
+        public string Nickname { get; set; }
+
+        /// <summary>
+        /// The player's real name
+        /// </summary>
+        public string RealName { get; set; }
+
+        #endregion
+
+
+        /// <summary>
+        /// Creates a new player
+        /// </summary>
+        /// <param name="nickname">The player's nickname</param>
+        /// <param name="realName">The player's real name</param>
+        /// <example>Player p = new Player("mycoolnick", "John Big");</example>
+        public Player(string nickname, string realName)
+        {
+            this.Nickname = nickname;
+            this.RealName = realName;
+        }
+    }
+}
