f434e6664fd4d9b804bb842c5ad144c9f818aa5e
asilva_sc@hotmail.com <asilva_sc@hotmail.com@6e8cf0fd-794f-984a-b2d7-fa5e9ce5254e>
2010-08-03 16:46:11 +0000

    Added GameLog and Board classes (actual Gamelog implementation is dubious, probably will change this later)

---
+++ Board.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Softklin.Checkers
+{
+    /// <summary>
+    /// Represents a game board for checkers game
+    /// </summary>
+    class Board
+    {
+        private const int BOARD_SIZE = 8;
+        private Piece[][] theBoard;
+    }
+}
---
+++ GameLog.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Softklin.Checkers
+{
+    /// <summary>
+    /// Represents a full game log
+    /// </summary>
+    class GameLog
+    {
+        private List<GameLogEntry> theLog;
+
+        public GameLog()
+        {
+            this.theLog = new List<GameLogEntry>();
+        }
+
+        public void logEvent(GameLogEntry glo)
+        {
+            this.theLog.Add(glo);
+        }
+    }
+
+    /// <summary>
+    /// Represents an entry in the game log
+    /// </summary>
+    class GameLogEntry
+    {
+        private DateTime time;
+        private LogEntryType type;
+    }
+
+    /// <summary>
+    /// Type of game log entry
+    /// </summary>
+    enum LogEntryType
+    {
+        GAME_START,
+        GAME_END,
+        VICTORY,
+        DEFEAT,
+        DRAW,
+        PLAYER_TURN,
+        PLAYER_PASS,
+        PIECE_MOVE,
+        PIECE_QUEEN,
+        PIECE_EAT
+    }
+}
